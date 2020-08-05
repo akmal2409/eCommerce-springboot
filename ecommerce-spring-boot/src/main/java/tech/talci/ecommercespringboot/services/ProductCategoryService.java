6d3a0400b327b1c37108b4322c30e9fc1ea6d56d
akmal2409 <akmalalikhujaev@gmail.com>
2020-08-05 16:03:02 +0200

    Implemented service layer for Product and ProductCategory

---
+++ ecommerce-spring-boot/src/main/java/tech/talci/ecommercespringboot/services/ProductCategoryService.java
@@ -0,0 +1,6 @@
+package tech.talci.ecommercespringboot.services;
+
+import tech.talci.ecommercespringboot.domain.ProductCategory;
+
+public interface ProductCategoryService extends CrudService<ProductCategory, Long> {
+}
---
+++ ecommerce-spring-boot/src/main/java/tech/talci/ecommercespringboot/services/ProductService.java
@@ -0,0 +1,6 @@
+package tech.talci.ecommercespringboot.services;
+
+import tech.talci.ecommercespringboot.domain.Product;
+
+public interface ProductService extends CrudService<Product, Long> {
+}
