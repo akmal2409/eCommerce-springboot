dc50107a76fbd20db7dbab269acff3ebae19d0c1
akmal2409 <akmalalikhujaev@gmail.com>
2020-08-05 16:36:17 +0200

    Temporary disabled HTTP requests such as DELETE, PUT, POST because for now only listing of products is required

---
+++ ecommerce-spring-boot/src/main/java/tech/talci/ecommercespringboot/configuration/DataRestConfig.java
@@ -0,0 +1,28 @@
+package tech.talci.ecommercespringboot.configuration;
+
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import tech.talci.ecommercespringboot.domain.Product;
+import tech.talci.ecommercespringboot.domain.ProductCategory;
+
+public class DataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+
+        HttpMethod[] unsupportedActions = {HttpMethod.DELETE, HttpMethod.PUT, HttpMethod.POST};
+
+        //disable HTTP methods defined in an array above
+
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+    }
+}
--- ecommerce-spring-boot/src/main/java/tech/talci/ecommercespringboot/repositories/ProductCategoryRepository.java
+++ ecommerce-spring-boot/src/main/java/tech/talci/ecommercespringboot/repositories/ProductCategoryRepository.java
@@ -1,9 +1,9 @@
 package tech.talci.ecommercespringboot.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import tech.talci.ecommercespringboot.domain.ProductCategory;
 
-@Repository
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
