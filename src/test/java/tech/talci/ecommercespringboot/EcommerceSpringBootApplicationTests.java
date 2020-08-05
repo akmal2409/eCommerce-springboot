faebfd08f15a4eba57d185ac6f30b62432c9ca06
akmal2409 <akmalalikhujaev@gmail.com>
2020-08-05 15:27:30 +0200

    Initial Commit

---
+++ src/test/java/tech/talci/ecommercespringboot/EcommerceSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.talci.ecommercespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
