6eb13cb5a922f5e49a7ddc84d3c988c82de924b9
akmal2409 <akmalalikhujaev@gmail.com>
2020-08-06 18:47:28 +0200

    Implemented method in ProductRepository findByCategoryId

--- ecommerce-spring-boot/src/main/java/tech/talci/ecommercespringboot/configuration/DataRestConfig.java
+++ ecommerce-spring-boot/src/main/java/tech/talci/ecommercespringboot/configuration/DataRestConfig.java
@@ -1,11 +1,13 @@
 package tech.talci.ecommercespringboot.configuration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import tech.talci.ecommercespringboot.domain.Product;
 import tech.talci.ecommercespringboot.domain.ProductCategory;
 
+@Configuration
 public class DataRestConfig implements RepositoryRestConfigurer {
 
     @Override
--- ecommerce-spring-boot/src/main/java/tech/talci/ecommercespringboot/repositories/ProductRepository.java
+++ ecommerce-spring-boot/src/main/java/tech/talci/ecommercespringboot/repositories/ProductRepository.java
@@ -1,8 +1,14 @@
 package tech.talci.ecommercespringboot.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 import tech.talci.ecommercespringboot.domain.Product;
 
-
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
