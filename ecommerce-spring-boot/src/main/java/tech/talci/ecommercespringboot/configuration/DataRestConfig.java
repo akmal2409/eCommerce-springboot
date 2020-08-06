d4c203c20809e5014ee3b3f6aa273539503990bb
akmal2409 <akmalalikhujaev@gmail.com>
2020-08-06 19:07:11 +0200

    Modified DataRestConfig to expose entity id's

--- ecommerce-spring-boot/src/main/java/tech/talci/ecommercespringboot/configuration/DataRestConfig.java
+++ ecommerce-spring-boot/src/main/java/tech/talci/ecommercespringboot/configuration/DataRestConfig.java
@@ -7,8 +7,20 @@ import org.springframework.http.HttpMethod;
 import tech.talci.ecommercespringboot.domain.Product;
 import tech.talci.ecommercespringboot.domain.ProductCategory;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class DataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    public DataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
@@ -26,5 +38,21 @@ public class DataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration configuration){
+
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        List<Class> entityClasses = new ArrayList<>();
+
+        for(EntityType entity: entities){
+            entityClasses.add(entity.getJavaType());
+        }
+
+        Class[] domainType = entityClasses.toArray(new Class[0]);
+        configuration.exposeIdsFor(domainType);
     }
 }
